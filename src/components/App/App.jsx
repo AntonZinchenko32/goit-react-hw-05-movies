392486cba717b7ab4877e1a1ffc33b2e9c5b00f8
AntonZinchenko32 <zinchenko309@gmail.com>
2023-09-28 20:28:02 +0300

    My_project_template_Step_1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { AppWrapper } from './App.styled';
+import { Global } from 'styles/Global.styled';
+
+export const App = () => {
+  return (
+    <>
+      <Global />
+      <AppWrapper>Hello world!</AppWrapper>
+    </>
+  );
+};
