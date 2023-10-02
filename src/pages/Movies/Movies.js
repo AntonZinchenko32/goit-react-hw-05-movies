41f1c668d9d532c0d5844121639d8672e0c32059
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-02 23:49:44 +0300

    Update Movies.js

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,45 +1,58 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import getMoviesBySearch from 'components/services/search-movies';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 import { MovieList } from 'components/MovieList/MovieList';
 
 const Movies = () => {
-  const [query, setQuery] = useState('');
+  const [queryForSubmit, setQueryForSubmit] = useState('');
 
   const [foundMovies, setFoundMovies] = useState([]);
 
   const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    // Записуємо дані з бекенду
-    const fetchMovies = async () => {
+
+  const query = searchParams.get('search') ?? '';
+
+  const ref = useRef(query)
+
+
+  // Записуємо дані з бекенду
+    const fetchMovies = useCallback(async searchText => {
       try {
-        const data = await getMoviesBySearch(query);
+        const data = await getMoviesBySearch(searchText);
 
         setFoundMovies(data.results);
       } catch (error) {
         console.log(error.message);
       }
-    };
-    query && fetchMovies();
-  }, [query]);
+    },[]) 
+
+  useEffect(() => {
+    queryForSubmit && fetchMovies(queryForSubmit);
+  }, [queryForSubmit, fetchMovies]);
+
+  useEffect(() => {
+    ref.current && fetchMovies(ref.current);
+  }, [fetchMovies]);
+
+
 
   const handleSearch = useCallback(value => {
-    setQuery(value);
+    setQueryForSubmit(value);
   }, []);
 
-  const movieTitle = searchParams.get('title') ?? '';
+  
 
-  const updateQueryString = title => {
-    const nextParams = title !== '' ? { title } : {};
+  const updateQueryString = search => {
+    const nextParams = search !== '' ? { search } : {};
     setSearchParams(nextParams);
   };
 
   return (
     <main>
       <SearchBar
-        value={movieTitle}
+        value={query}
         onChange={updateQueryString}
         onSubmit={handleSearch}
       />
