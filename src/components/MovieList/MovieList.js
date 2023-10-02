984e7cb63e60961706ef2f6aff081bd8dd3b2f8b
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-02 23:54:19 +0300

    Update MovieList.js

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -10,7 +10,7 @@ export const MovieList = ({ movies }) => {
         {movies.map((movie) => (
           <CardWrapper key={movie.id}>
             <Link to={`${movie.id}`} state={{ from: location }}>
-              <MovieTitle>{movie.title}</MovieTitle>
+              <MovieTitle>{movie.title ?? movie.name}</MovieTitle>
             </Link>
           </CardWrapper>
         ))}
