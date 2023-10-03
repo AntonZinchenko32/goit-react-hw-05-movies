e6515d12699616d64fe1226362b4d799d3653929
AntonZinchenko32 <98322005+AntonZinchenko32@users.noreply.github.com>
2023-10-03 11:38:00 +0000

    Deploying to gh-pages from @ AntonZinchenko32/goit-react-hw-05-movies@df070a428b8977b1a83c6110cc30627854a9a140 🚀

---
+++ static/js/541.e13120d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[541],{541:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h1",{children:"Hello"})})}}}]);
+//# sourceMappingURL=541.e13120d9.chunk.js.map
