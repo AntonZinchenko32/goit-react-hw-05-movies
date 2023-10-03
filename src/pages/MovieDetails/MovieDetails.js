c2b4efa22280dedfec0ec2ee1d787efb21f0b1d0
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-03 03:50:59 +0300

    Realizes the poster display

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -1,12 +1,14 @@
-import { useParams, useLocation} from "react-router-dom";
-import { BackLink } from "components/BackLink/BackLink";
-import getMovieById from "components/services/get-movie-details";
-import { useState, useEffect } from "react";
-
+import { useParams, useLocation } from 'react-router-dom';
+import { BackLink } from 'components/BackLink/BackLink';
+import getMovieById from 'components/services/get-movie-details';
+import { useState, useEffect } from 'react';
+import { MovieDetailsBox, MovieImage } from './MovieDetails.styled';
 
 const MovieDetails = () => {
+  const [movie, setMovie] = useState(null);
 
-  const [movie, setMovie] = useState(null)
+  const defaultImg =
+    'https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg';
 
   const { movieId } = useParams();
 
@@ -15,7 +17,7 @@ const MovieDetails = () => {
     const fetchMovie = async () => {
       try {
         const data = await getMovieById(movieId);
-        
+        console.log(data);
         setMovie(data);
       } catch (error) {
         console.log(error.message);
@@ -25,27 +27,42 @@ const MovieDetails = () => {
   }, [movieId]);
 
   const location = useLocation();
-  const backLinkHref = location.state?.from ?? "/movies";
-  
+  const backLinkHref = location.state?.from ?? '/movies';
 
   return (
-      <main>
-        <BackLink to={backLinkHref}>Back to movies</BackLink>
-        <img src="https://via.placeholder.com/960x240" alt="" />
-        <div>
-          {movie && <h2>
-            Movie - {movie.title} - {movieId}
-          </h2>}
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
-            sunt excepturi nesciunt iusto dignissimos assumenda ab quae cupiditate
-            a, sed reprehenderit? Deleniti optio quasi, amet natus reiciendis
-            atque fuga dolore? Lorem, ipsum dolor sit amet consectetur adipisicing
-            elit. Impedit suscipit quisquam incidunt commodi fugiat aliquam
-            praesentium ipsum quos unde voluptatum?
-          </p>
-        </div>
-      </main>
+    <main>
+      {movie && (
+        <>
+          <BackLink to={backLinkHref}>Back to movies</BackLink>
+          <MovieDetailsBox>
+            <MovieImage
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : defaultImg
+              }
+              width={250}
+              alt="poster"
+            />
+            <div>
+              {movie && (
+                <h2>
+                  Movie - {movie.title} - {movieId}
+                </h2>
+              )}
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloribus sunt excepturi nesciunt iusto dignissimos assumenda ab
+                quae cupiditate a, sed reprehenderit? Deleniti optio quasi, amet
+                natus reiciendis atque fuga dolore? Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Impedit suscipit quisquam incidunt
+                commodi fugiat aliquam praesentium ipsum quos unde voluptatum?
+              </p>
+            </div>
+          </MovieDetailsBox>
+        </>
+      )}
+    </main>
   );
 };
 
--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+export const MovieImage = styled.img`
+  margin-right: 20px;
+`;
+
+export const MovieDetailsBox = styled.div`
+  display: flex;
+`;
